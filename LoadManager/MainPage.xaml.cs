e7849c574260edbada7d013d9922ece17ae43509
Santiago Daniel <santiagodpp@outlook.com>
2026-06-03 11:40:23 -0600

    Corrige comportamiento mobile y validaciones de comunicacion

--- LoadManager/MainPage.xaml.cs
+++ LoadManager/MainPage.xaml.cs
@@ -1,10 +1,34 @@
-﻿namespace LoadManager
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoadManager;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        public MainPage()
+        InitializeComponent();
+
+        if (DeviceInfo.Platform == DevicePlatform.Android)
         {
-            InitializeComponent();
+            Padding = new Thickness(0, 0, 0, 12);
         }
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        _ = blazorWebView.TryDispatchAsync(services =>
+        {
+            var navigation = services.GetRequiredService<NavigationManager>();
+            var relativePath = navigation.ToBaseRelativePath(navigation.Uri);
+
+            if (!string.IsNullOrWhiteSpace(relativePath) &&
+                !relativePath.Equals("despacho", StringComparison.OrdinalIgnoreCase))
+            {
+                navigation.NavigateTo("/");
+            }
+        });
+
+        return true;
     }
 }
